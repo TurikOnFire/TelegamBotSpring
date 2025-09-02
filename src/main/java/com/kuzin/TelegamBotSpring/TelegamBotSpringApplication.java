0e9afdde2116fd0332e566923c54cda2bf037ae5
Arthur K <artyrking13@gmail.com>
2025-09-02 11:10:49 +0300

    Initial commit

---
+++ src/main/java/com/kuzin/TelegamBotSpring/TelegamBotSpringApplication.java
@@ -0,0 +1,13 @@
+package com.kuzin.TelegamBotSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegamBotSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegamBotSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kuzin/TelegamBotSpring/TelegamBotSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuzin.TelegamBotSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegamBotSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
